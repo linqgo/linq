e70d890380fdcb759adb86f1f0f59ff5740d134c
Marcelo Cantos <marcelo.cantos@gmail.com>
2022-08-10 19:23:52 +1000

    linter fixes

--- default_test.go
+++ default_test.go
@@ -3,12 +3,13 @@ package linq_test
 import (
 	"testing"
 
-	"github.com/marcelocantos/linq"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/marcelocantos/linq"
 )
 
 func TestDefaultIfEmpty(t *testing.T) {
 	t.Parallel()
 
-	assert.Equal(t, []int{42}, linq.From[int]().DefaultIfEmpty(42))
+	assert.Equal(t, []int{42}, linq.From[int]().DefaultIfEmpty(42).ToSlice())
 }
--- keyvalue.go
+++ keyvalue.go
@@ -1,12 +1,15 @@
 package linq
 
 // KV represents a key/value pair.
-type KV[K comparable, V any] struct {
+type KV[K, V any] struct {
 	Key   K
 	Value V
 }
 
 // NewKV returns a new KV.
-func NewKV[K comparable, V any](key K, value V) KV[K, V] {
-	return KV[K, V]{Key: key, Value: value}
+func NewKV[K, V any](key K, value V) KV[K, V] {
+	return KV[K, V]{
+		Key:   key,
+		Value: value,
+	}
 }
