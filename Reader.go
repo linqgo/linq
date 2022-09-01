8c25379d3f391791783ed5d2889e96f32241e3b8
Marcelo Cantos <marcelo.cantos@gmail.com>
2022-09-01 17:33:01 +1000

    Support io readers as queries

---
+++ Reader.go
@@ -0,0 +1,80 @@
+package linq
+
+import (
+	"bufio"
+	"io"
+)
+
+// FromByteReader returns a query containing bytes read from r. Hint: use a
+// bufio.Reader to wrap an io.Reader in an io.ByteReader.
+//
+// The returned query is not replayable. Use (Query).Memoize() if you need a
+// replayable query.
+func FromByteReader(r io.ByteReader) Query[byte] {
+	return readerQuery(r.ReadByte)
+}
+
+// FromRuneReader returns a query containing runes read from r. Hint: use a
+// bufio.Reader to wrap an io.Reader in an io.RuneReader.
+//
+// The returned query is not replayable. Use (Query).Memoize() if you need a
+// replayable query.
+func FromRuneReader(r io.RuneReader) Query[rune] {
+	return readerQuery(func() (rune, error) {
+		c, _, err := r.ReadRune()
+		return c, err
+	})
+}
+
+// FromScanner reads a query containing []byte tokens read from s. Hint: use
+// (Scanner).Split() to control how the input stream is tokenized.
+func FromScanner(s *bufio.Scanner) Query[[]byte] {
+	return NewQuery(func() Enumerator[[]byte] {
+		return readerEnumerator(func() ([]byte, error) {
+			switch {
+			case s.Scan():
+				return s.Bytes(), nil
+			case s.Err() == nil:
+				return nil, io.EOF
+			default:
+				return nil, s.Err()
+			}
+		})
+	}).withOneShot(true)
+}
+
+// FromScannerString reads a query containing string tokens read from s. Hint: use
+// (Scanner).Split() to control how the input stream is tokenized.
+func FromScannerString(r *bufio.Scanner) Query[string] {
+	return NewQuery(func() Enumerator[string] {
+		return readerEnumerator(func() (string, error) {
+			switch {
+			case r.Scan():
+				return r.Text(), nil
+			case r.Err() == nil:
+				return "", io.EOF
+			default:
+				return "", r.Err()
+			}
+		})
+	}).withOneShot(true)
+}
+
+func readerQuery[T any](read func() (T, error)) Query[T] {
+	return NewQuery(func() Enumerator[T] {
+		return readerEnumerator(read)
+	}).withOneShot(true)
+}
+
+func readerEnumerator[T any](read func() (T, error)) Enumerator[T] {
+	return func() (T, bool) {
+		c, err := read()
+		if err == nil {
+			return c, true
+		}
+		if err != io.EOF {
+			panic(err)
+		}
+		return c, false
+	}
+}
---
+++ Reader_test.go
@@ -0,0 +1,66 @@
+package linq_test
+
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/marcelocantos/linq"
+)
+
+func TestReaderByte(t *testing.T) {
+	t.Parallel()
+
+	s := bufio.NewReader(bytes.NewBuffer([]byte("hello")))
+	q := linq.FromByteReader(s)
+	assertQueryEqual(t, []byte{'h', 'e', 'l', 'l', 'o'}, q)
+
+	assertOneShot(t, true, q)
+}
+
+func TestRuneReaderRune(t *testing.T) {
+	t.Parallel()
+
+	s := bufio.NewReader(bytes.NewBuffer([]byte("太容易!")))
+	q := linq.FromRuneReader(s)
+	assertQueryEqual(t, []rune{'太', '容', '易', '!'}, q)
+
+	assertOneShot(t, true, q)
+}
+
+func TestScanner(t *testing.T) {
+	t.Parallel()
+
+	s := bufio.NewScanner(bytes.NewBuffer([]byte("hello\nworld\n")))
+	q := linq.FromScanner(s)
+	assertQueryEqual(t, [][]byte{[]byte("hello"), []byte("world")}, q)
+
+	assertOneShot(t, true, q)
+}
+
+func TestScannerText(t *testing.T) {
+	t.Parallel()
+
+	s := bufio.NewScanner(bytes.NewBuffer([]byte("hello\nworld\n")))
+	q := linq.FromScannerString(s)
+	assertQueryEqual(t, []string{"hello", "world"}, q)
+
+	assertOneShot(t, true, q)
+}
+
+func TestBadReader(t *testing.T) {
+	t.Parallel()
+
+	r := bufio.NewScanner(badReader{})
+	assert.Panics(t, func() { linq.FromScanner(r).ToSlice() })
+	assert.Panics(t, func() { linq.FromScannerString(r).ToSlice() })
+}
+
+type badReader struct{}
+
+func (badReader) Read([]byte) (int, error) {
+	return 0, errors.New("oops")
+}
