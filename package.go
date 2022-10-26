e13549007b3ee0e85cfb343cbbeaad441f983112
Marcelo Cantos <marcelo.cantos@gmail.com>
2022-10-26 20:48:29 +1100

    Add license notice to all sources

--- package.go
+++ package.go
@@ -1,3 +1,17 @@
+// Copyright 2022 Marcelo Cantos
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0//
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Package linq is a data manipulation library inspired by C# Linq.
 //
 // See the [Catalog] for a detailed functional overview of the library.
@@ -43,4 +57,17 @@
 // by .Net.
 //
 // [Catalog]: https://github.com/linqgo/linq/blob/main/doc/catalog.md
+// Copyright 2022 Marcelo Cantos
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0//
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package linq
