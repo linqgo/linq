fec2662ed56aab8eefffecca890127aa240244fb
Marcelo Cantos <marcelo.cantos@gmail.com>
2024-04-21 12:26:50 +1000

    update copyright from 2022 to 2022-2024

--- default.go
+++ default.go
@@ -1,4 +1,4 @@
-// Copyright 2022 Marcelo Cantos
+// Copyright 2022-2024 Marcelo Cantos
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
